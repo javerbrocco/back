3987a205e9047a9530a4d5f918e60b9f32ca9a33
Walterm99 <142105338+javerbrocco@users.noreply.github.com>
2023-08-18 12:37:30 -0300

    Update AuthController.java

--- src/main/java/com/portfolio/jvb/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/jvb/Security/Controller/AuthController.java
@@ -36,7 +36,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://frontend-46920.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-46920.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
